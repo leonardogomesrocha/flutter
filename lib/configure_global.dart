076bd3e5b7a4366bb2df510e6df62e6879615942
Leonardo Rocha <leonardo.rocha@itmagination.com>
2026-06-11 22:46:21 +0100

    Update configure_global.dart

--- lib/configure_global.dart
+++ lib/configure_global.dart
@@ -3,7 +3,7 @@ class ConfigureGlobal {
   ConfigureGlobal._();
 
   // Base URL of your API
-  static const String apiBaseUrl = 'https://sport-api-4.onrender.com';
+  static const String apiBaseUrl = 'https://syrcgate.com';
   ///static const String apiBaseUrl = 'http://localhost:5003';
   // Basic Authorization header value
   static const String basicAuth = 'Basic YXBpdXNlcjoxMjM0NTY=';
